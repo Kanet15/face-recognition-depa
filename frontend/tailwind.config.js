72f09553ebd87571db2d0193eb72a0d6723aea14
Kanet Chusangthong <107930657+Kanet15@users.noreply.github.com>
2025-09-28 20:26:30 +0700

    Delete frontend/tailwind.config.js

--- frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class', // enable toggling via the 'dark' class on <html>
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
